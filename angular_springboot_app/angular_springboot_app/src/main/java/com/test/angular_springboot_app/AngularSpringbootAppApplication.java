19b64091e0047370334dd759e2348dc9e32bf389
biofreak89 <bolasgiorgos@gmail.com>
2024-06-08 16:08:09 +0300

    init angular and springboot apps
    eginan installed angular, springboot
    pending: configuration

---
+++ angular_springboot_app/angular_springboot_app/src/main/java/com/test/angular_springboot_app/AngularSpringbootAppApplication.java
@@ -0,0 +1,13 @@
+package com.test.angular_springboot_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSpringbootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularSpringbootAppApplication.class, args);
+	}
+
+}
---
+++ angular_springboot_app/angular_springboot_app/src/test/java/com/test/angular_springboot_app/AngularSpringbootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.angular_springboot_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularSpringbootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
